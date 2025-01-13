8150f1796ed4c8309c61096b1618e7aa7bcca1f9
taofeekaj <taofeekaj@examroom.ai>
2025-01-13 21:53:23 +0100

    entities added

--- Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
+++ Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b76b23c29f297b6e5cb5954b4af82c6ea9e4e195")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+762e50f587424efa14086c382414cd805b56252f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
