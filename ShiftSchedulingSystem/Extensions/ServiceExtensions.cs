04c880cf96510a40e7b4145013d6a91a96998810
taofeekaj <taofeekaj@examroom.ai>
2025-01-14 16:49:45 +0100

    CORS Policy added

--- ShiftSchedulingSystem/Extensions/ServiceExtensions.cs
+++ ShiftSchedulingSystem/Extensions/ServiceExtensions.cs
@@ -9,6 +9,19 @@ public static class ServiceExtensions
 {
     public static void AddComponents(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy", builder =>
+              builder.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .WithExposedHeaders("X-Pagination"));
+        });
+
+        services.Configure<IISOptions>(options =>
+        {
+        });
+
         services.AddSingleton<ILoggerManager, LoggerManager>();
 
         services.AddScoped<IRepositoryManager, RepositoryManager>();
--- ShiftSchedulingSystem/Program.cs
+++ ShiftSchedulingSystem/Program.cs
@@ -24,6 +24,8 @@ using (var scope = app.Services.CreateScope())
 
 app.UseHttpsRedirection();
 
+app.UseCors("CorsPolicy");
+
 app.UseAuthorization();
 
 app.MapControllers();
